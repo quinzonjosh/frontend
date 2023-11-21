4520c7d2e560d70b15076f2a493b1d719b0d9256
quinzonjosh <quinzonjosh261@gmail.com>
2023-11-22 02:22:29 +0800

    implemented navbar

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import { render } from 'preact'
 import { App } from './app.jsx'
-import './index.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 render(<App />, document.getElementById('app'))
